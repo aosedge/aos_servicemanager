06a742be3d745b8bac268d0c8109815f27773d07
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-09-12 15:54:23 +0300

    [scripts] Move service name and working dir as parameters of cleanup.sh
    In order to generalize cleanup script, systemd service name and working
    directory should be passed as command line parameters. Because on different
    platform systemd service could have different name and different working
    directory.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- misc/scripts/cleanup.sh
+++ misc/scripts/cleanup.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+print_usage()
+{
+	echo "Usage: `basename $0` <servicemanager> <workingdir>"
+	echo "    <servicemanager> - systemd service manager service"
+	echo "    <workingdir> - path to service manager working directory"
+}
+
+if [ "$1" == "-h" ]; then
+	print_usage
+	exit 0
+fi
+
+if [ "$#" -ne 2 ]; then
+	print_usage
+	exit 1
+fi
+
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
 fi
 
-echo Stopping aos_servicemanager...
-systemctl stop aos_servicemanager
+echo Stopping $1...
+systemctl stop $1
 if [ $? != 0 ]; then
-	echo "Can't stop aos_servicemanager" 1>&2
+	echo "Can't stop $1" 1>&2
 fi
 echo Stopped
 
-for f in data/services/* ; do
+for f in $2/services/* ; do
 	if [ -d "$f" ]; then
 		r=$(basename $(find $f -path '*.service'))
 		if [ ! -z "$r" ]; then
@@ -28,17 +45,17 @@ for f in data/services/* ; do
 done
 
 echo Removing services folder...
-rm data/services -rf
+rm $2/services -rf
 echo Removed
 
 echo Removing database...
-rm data/services.db
+rm $2/services.db
 echo Removed
 
 echo
 echo =========================================================================
-echo WARNING: aos_servicemanager is stopped. To start it use following command:
-echo '	sudo systemctl start aos_servicemanager'
+echo WARNING: $1 is stopped. To start it use following command:
+echo '	sudo systemctl start $1'
 echo or:
 echo '	sudo reboot'
 echo ':)'
