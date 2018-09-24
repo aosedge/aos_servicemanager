023e6f98ddd77d03efd579bb343a95860f3386ab
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-09-24 13:45:26 +0300

    [monitoring] Implement getting and sending system parameters
    Implement functions to get system RAM, CPU and Disk usage with
    github.com/shirou/gopsutil package. Add two timers: pollTimer to get
    data with pollPerid and sendTimer to send data with sendPeriod.
    
    Signed-off-by: Yevgen Abramov <yevgen_abramov@epam.com>
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- monitoring/monitoring.go
+++ monitoring/monitoring.go
@@ -2,6 +2,14 @@
 package monitoring
 
 import (
+	"errors"
+	"math"
+	"sync"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
 	log "github.com/sirupsen/logrus"
 
 	amqp "gitpct.epam.com/epmd-aepr/aos_servicemanager/amqphandler"
@@ -29,6 +37,15 @@ type ServiceMonitoringItf interface {
 // Monitor instance
 type Monitor struct {
 	DataChannel chan amqp.MonitoringData
+
+	config     config.Monitoring
+	workingDir string
+
+	sendTimer *time.Ticker
+	pollTimer *time.Ticker
+
+	mutex      sync.Mutex
+	dataToSend amqp.MonitoringData
 }
 
 // ServiceMonitoringConfig contains info about service and rules for monitoring alerts
@@ -39,6 +56,13 @@ type ServiceMonitoringConfig struct {
 }
 
 /*******************************************************************************
+ * Variable
+ ******************************************************************************/
+
+// ErrDisabled indicates that monitoring is disable in the config
+var ErrDisabled = errors.New("Monitoring is disabled")
+
+/*******************************************************************************
  * Public
  ******************************************************************************/
 
@@ -46,9 +70,21 @@ type ServiceMonitoringConfig struct {
 func New(config *config.Config) (monitor *Monitor, err error) {
 	log.Debug("Create monitor")
 
+	if config.Monitoring.Disabled {
+		return nil, ErrDisabled
+	}
+
 	monitor = &Monitor{}
 
 	monitor.DataChannel = make(chan amqp.MonitoringData, dataChannelSize)
+
+	monitor.config = config.Monitoring
+	monitor.workingDir = config.WorkingDir
+
+	monitor.pollTimer = time.NewTicker(monitor.config.PollPeriod.Duration)
+	monitor.sendTimer = time.NewTicker(monitor.config.SendPeriod.Duration)
+
+	go monitor.run()
 
 	return monitor, nil
 }
@@ -56,6 +92,9 @@ func New(config *config.Config) (monitor *Monitor, err error) {
 // Close closes monitor instance
 func (monitor *Monitor) Close() {
 	log.Debug("Close monitor")
+
+	monitor.sendTimer.Stop()
+	monitor.pollTimer.Stop()
 }
 
 // StartMonitorService starts monitoring service
@@ -70,4 +109,82 @@ func (monitor *Monitor) StopMonitorService(serviceID string) (err error) {
 	log.WithField("id", serviceID).Debug("Stop service monitoring")
 
 	return nil
+}
+
+/*******************************************************************************
+ * Private
+ ******************************************************************************/
+
+func (monitor *Monitor) run() error {
+	for {
+		select {
+		case <-monitor.sendTimer.C:
+			monitor.mutex.Lock()
+			monitor.sendMonitoringData()
+			monitor.mutex.Unlock()
+
+		case <-monitor.pollTimer.C:
+			monitor.mutex.Lock()
+			monitor.getCurrentSystemData()
+			monitor.mutex.Unlock()
+		}
+	}
+}
+
+func (monitor *Monitor) sendMonitoringData() {
+	monitor.DataChannel <- monitor.dataToSend
+}
+
+func (monitor *Monitor) getCurrentSystemData() {
+	var err error
+
+	monitor.dataToSend.Global.CPU, err = getSystemCPUUsage()
+	if err != nil {
+		log.Errorf("Can't get system CPU: %s", err)
+	}
+
+	monitor.dataToSend.Global.RAM, err = getSystemRAMUsage()
+	if err != nil {
+		log.Errorf("Can't get system RAM: %s", err)
+	}
+
+	monitor.dataToSend.Global.UsedDisk, err = getSystemDiskUsage(monitor.workingDir)
+	if err != nil {
+		log.Errorf("Can't get system Disk usage: %s", err)
+	}
+
+	log.WithFields(log.Fields{
+		"CPU":  monitor.dataToSend.Global.CPU,
+		"RAM":  monitor.dataToSend.Global.RAM,
+		"Disk": monitor.dataToSend.Global.UsedDisk}).Debug("Monitoring data")
+}
+
+// getSystemCPUUsage returns CPU usage in parcent
+func getSystemCPUUsage() (cpuUse uint64, err error) {
+	v, err := cpu.Percent(0, false)
+	if err != nil {
+		return cpuUse, err
+	}
+
+	return uint64(math.RoundToEven(v[0])), nil
+}
+
+// getSystemRAMUsage returns RAM usage in bytes
+func getSystemRAMUsage() (ram uint64, err error) {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		return ram, err
+	}
+
+	return v.Used, nil
+}
+
+// getSystemDiskUsage returns disc usage in bytes
+func getSystemDiskUsage(path string) (discUse uint64, err error) {
+	v, err := disk.Usage(path)
+	if err != nil {
+		return discUse, err
+	}
+
+	return v.Used, nil
 }
