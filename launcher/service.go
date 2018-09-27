2987cc0e1b3b4d3d39c31e969cde7c382c71dc84
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-09-27 16:24:16 +0300

    [launcher] Ignore ret code of wondershaper in systemd template service file
    Wondershaper returns non zero code even for normal execution. Systemd
    breaks start and stop procedure in this case. To resolve this issue ret
    code of wondershaper should be disabled in systemd service file.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- launcher/service.go
+++ launcher/service.go
@@ -347,9 +347,9 @@ Restart=always
 RestartSec=1
 ExecStartPre=${RUNC} delete -f ${ID}
 ExecStart=${RUNC} run -d --pid-file ${SERVICEPATH}/${ID}.pid -b ${SERVICEPATH} ${ID}
-ExecStartPost=${SETNETLIMIT}
+ExecStartPost=-${SETNETLIMIT}
 
-ExecStop=${CLEARNETLIMIT}
+ExecStop=-${CLEARNETLIMIT}
 ExecStop=${RUNC} kill ${ID} SIGKILL
 ExecStopPost=${RUNC} delete -f ${ID}
 PIDFile=${SERVICEPATH}/${ID}.pid
