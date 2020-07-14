2a0435e7ba949a3ebd4314a23489ce370910dc99
Yevgen Abramov <Yevgen_Abramov@epam.com>
2020-07-14 10:42:37 +0300

    [ci] Update ci setup script
    Signed-off-by: Yevgen Abramov <Yevgen_Abramov@epam.com>

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
-echo "Install  Golang v 1.12"
+echo "Install  Golang v 1.14"
 
-curl https://dl.google.com/go/go1.12.16.linux-amd64.tar.gz --output /tmp/go.tar.gz
+curl https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz --output /tmp/go.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go.tar.gz
 echo  PATH=$PATH:/usr/local/go/bin >> $HOME/.profile
-source $HOME/.profile
 
 mkdir -p $HOME/go/src
 
-sudo apt install libsystemd-dev -y
-sudo apt install dbus-x11
+echo "Install SM dependencies"
+sudo apt update -y
+sudo apt install libsystemd-dev dbus-x11 make iperf quota runc -y
 
-echo "Install  netns"
-
-export NETNS_SHA256="8a3a48183ed5182a0619b18f05ef42ba5c4c3e3e499a2e2cb33787bd7fbdaa5c"
-# Download and check the sha256sum.
-sudo curl -fSL "https://github.com/genuinetools/netns/releases/download/v0.5.3/netns-linux-amd64" -o "/usr/local/bin/netns" \
-	&& echo "${NETNS_SHA256}  /usr/local/bin/netns" | sha256sum -c - \
-	&& sudo chmod a+x "/usr/local/bin/netns"
-
+echo "Install wondershaper"
 cd /tmp
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 sudo make install
 
-sudo apt install iperf
+echo "Install tests dependencies"
+sudo apt install libssl-dev -y
+sudo apt install rabbitmq-server -y
+sudo systemctl disable rabbitmq-server
+
+sudo apt install python3-pip -y
+sudo -H pip3 install pyftpdlib
+
+echo "Install gitlab runner dependencies"
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+sudo apt-get install gitlab-runner
+sudo usermod -a -G sudo gitlab-runner
