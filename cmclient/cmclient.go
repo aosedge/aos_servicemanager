b7b297b531c280228da9d25e7c544672e6ae9c44
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2020-09-10 17:59:58 +0300

    [cmclient] Implement CM client with unit tests
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ cmclient/cmclient.go
@@ -0,0 +1,218 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 Renesas Inc.
+// Copyright 2020 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmclient
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/base64"
+	"errors"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	pb "gitpct.epam.com/epmd-aepr/aos_common/api/certificatemanager"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	amqp "aos_servicemanager/amqphandler"
+	"aos_servicemanager/config"
+	"aos_servicemanager/fcrypt"
+)
+
+/*******************************************************************************
+ * Consts
+ ******************************************************************************/
+
+const (
+	cmRequestTimeout = 30 * time.Second
+)
+
+/*******************************************************************************
+ * Types
+ ******************************************************************************/
+
+// Client CM client instance
+type Client struct {
+	sender     Sender
+	connection *grpc.ClientConn
+	pbclient   pb.CertificateManagerClient
+}
+
+// Sender provides API to send messages to the cloud
+type Sender interface {
+	SendIssueUnitCertificatesRequest(requests []amqp.CertificateRequest) (err error)
+	SendInstallCertificatesConfirmation(confirmations []amqp.CertificateConfirmation) (err error)
+}
+
+/*******************************************************************************
+ * Public
+ ******************************************************************************/
+
+// New creates new CM client
+func New(config *config.Config, sender Sender, insecure bool) (client *Client, err error) {
+	if sender == nil {
+		return nil, errors.New("sender is nil")
+	}
+
+	client = &Client{sender: sender}
+
+	log.Debug("Connecting to CM...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), cmRequestTimeout)
+	defer cancel()
+
+	var secureOpt grpc.DialOption
+
+	if insecure {
+		secureOpt = grpc.WithInsecure()
+	} else {
+		secureOpt = grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: false}))
+	}
+
+	if client.connection, err = grpc.DialContext(ctx, config.CMServerURL, secureOpt, grpc.WithBlock()); err != nil {
+		return nil, err
+	}
+
+	client.pbclient = pb.NewCertificateManagerClient(client.connection)
+
+	log.Debug("Connected to CM")
+
+	return client, nil
+}
+
+// RenewCertificatesNotification renew certificates notification
+func (client *Client) RenewCertificatesNotification(systemID, pwd string, certInfo []amqp.CertificateNotification) (err error) {
+	var newCerts []amqp.CertificateRequest
+
+	for _, cert := range certInfo {
+		log.WithFields(log.Fields{"type": cert.Type, "serial": cert.Serial, "validTill": cert.ValidTill}).Debug("Renew certificate")
+
+		ctx, cancel := context.WithTimeout(context.Background(), cmRequestTimeout)
+		defer cancel()
+
+		request := &pb.CreateKeysReq{Type: cert.Type, SystemId: systemID, Password: pwd}
+
+		response, err := client.pbclient.CreateKeys(ctx, request)
+		if err != nil {
+			return err
+		}
+
+		if response.Error != "" {
+			return errors.New(response.Error)
+		}
+
+		newCerts = append(newCerts, amqp.CertificateRequest{Type: response.Type, Csr: response.Csr})
+	}
+
+	if len(newCerts) == 0 {
+		return nil
+	}
+
+	if err := client.sender.SendIssueUnitCertificatesRequest(newCerts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// InstallCertificates applies new issued certificates
+func (client *Client) InstallCertificates(certInfo []amqp.IssuedUnitCertificatesInfo) (err error) {
+	var confirmations []amqp.CertificateConfirmation
+
+	for _, cert := range certInfo {
+		log.WithFields(log.Fields{"type": cert.Type}).Debug("Install certificate")
+
+		ctx, cancel := context.WithTimeout(context.Background(), cmRequestTimeout)
+		defer cancel()
+
+		request := &pb.ApplyCertReq{Type: cert.Type, Cert: cert.CertificateChain}
+		certConfirmation := amqp.CertificateConfirmation{Type: cert.Type}
+
+		response, err := client.pbclient.ApplyCert(ctx, request)
+
+		if response.GetError() != "" {
+			err = errors.New(response.Error)
+		}
+
+		if err == nil {
+			certConfirmation.Serial, err = fcrypt.GetCrtSerialByURL(response.CertUrl)
+		}
+
+		if err == nil {
+			certConfirmation.Status = "installed"
+		} else if err != nil {
+			certConfirmation.Status = "not installed"
+			certConfirmation.Description = err.Error()
+
+			log.WithFields(log.Fields{"type": cert.Type}).Errorf("Can't install certificate: %s", err)
+		}
+
+		confirmations = append(confirmations, certConfirmation)
+	}
+
+	if len(confirmations) == 0 {
+		return nil
+	}
+
+	if err = client.sender.SendInstallCertificatesConfirmation(confirmations); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetCertificate gets certificate by issuer
+func (client *Client) GetCertificate(certType string, issuer []byte, serial string) (certURL, keyURL string, err error) {
+	log.WithFields(log.Fields{
+		"type":   certType,
+		"issuer": base64.StdEncoding.EncodeToString(issuer),
+		"serial": serial}).Debug("Get certificate")
+
+	ctx, cancel := context.WithTimeout(context.Background(), cmRequestTimeout)
+	defer cancel()
+
+	request := &pb.GetCertReq{Type: certType, Issuer: issuer, Serial: serial}
+
+	response, err := client.pbclient.GetCert(ctx, request)
+	if err != nil {
+		return "", "", err
+	}
+
+	if response.Error != "" {
+		return "", "", errors.New(response.Error)
+	}
+
+	log.WithFields(log.Fields{"certURL": response.CertUrl, "keyURL": response.KeyUrl}).Debug("Certificate info")
+
+	return response.CertUrl, response.KeyUrl, nil
+}
+
+// Close closes CM client
+func (client *Client) Close() (err error) {
+	log.Debug("Disconnected from CM")
+
+	if client.connection != nil {
+		client.connection.Close()
+	}
+
+	return nil
+}
+
+/*******************************************************************************
+ * Private
+ ******************************************************************************/
---
+++ cmclient/cmclient_test.go
@@ -0,0 +1,358 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 Renesas Inc.
+// Copyright 2020 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmclient_test
+
+import (
+	"context"
+	"io/ioutil"
+	"net"
+	"net/url"
+	"os"
+	"path"
+	"reflect"
+	"strconv"
+	"testing"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	pb "gitpct.epam.com/epmd-aepr/aos_common/api/certificatemanager"
+	"google.golang.org/grpc"
+
+	amqp "aos_servicemanager/amqphandler"
+	"aos_servicemanager/cmclient"
+	"aos_servicemanager/config"
+)
+
+/*******************************************************************************
+ * Consts
+ ******************************************************************************/
+
+const serverURL = "localhost:8089"
+
+/*******************************************************************************
+ * Types
+ ******************************************************************************/
+
+type testServer struct {
+	grpcServer *grpc.Server
+	csr        map[string]string
+	certURL    map[string]string
+	keyURL     map[string]string
+}
+
+type testSender struct {
+	csr    map[string]string
+	serial map[string]string
+}
+
+/*******************************************************************************
+ * Vars
+ ******************************************************************************/
+
+var tmpDir string
+
+/*******************************************************************************
+ * Init
+ ******************************************************************************/
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: false,
+		TimestampFormat:  "2006-01-02 15:04:05.000",
+		FullTimestamp:    true})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+}
+
+/*******************************************************************************
+ * Main
+ ******************************************************************************/
+
+func TestMain(m *testing.M) {
+	var err error
+
+	tmpDir, err = ioutil.TempDir("", "cm_")
+	if err != nil {
+		log.Fatalf("Error create temporary dir: %s", err)
+	}
+
+	ret := m.Run()
+
+	if err := os.RemoveAll(tmpDir); err != nil {
+		log.Fatalf("Error removing tmp dir: %s", err)
+	}
+
+	os.Exit(ret)
+}
+
+/*******************************************************************************
+ * Tests
+ ******************************************************************************/
+
+func TestRenewCertificatesNotification(t *testing.T) {
+	sender := &testSender{}
+
+	server, err := newTestServer(serverURL)
+	if err != nil {
+		t.Fatalf("Can't create test server: %s", err)
+	}
+	defer server.close()
+
+	server.csr = map[string]string{"online": "onlineCSR", "offline": "offlineCSR"}
+
+	client, err := cmclient.New(&config.Config{CMServerURL: serverURL}, sender, true)
+	if err != nil {
+		t.Fatalf("Can't create CM client: %s", err)
+	}
+	defer client.Close()
+
+	certInfo := []amqp.CertificateNotification{
+		{Type: "online", Serial: "serail1", ValidTill: time.Now()},
+		{Type: "offline", Serial: "serail2", ValidTill: time.Now()},
+	}
+
+	if err = client.RenewCertificatesNotification("testID", "pwd", certInfo); err != nil {
+		t.Fatalf("Can't process renew certificate notification: %s", err)
+	}
+
+	if !reflect.DeepEqual(server.csr, sender.csr) {
+		t.Errorf("Wrong sender CSR: %v", sender.csr)
+	}
+}
+
+func TestInstallCertificates(t *testing.T) {
+	sender := &testSender{}
+
+	server, err := newTestServer(serverURL)
+	if err != nil {
+		t.Fatalf("Can't create test server: %s", err)
+	}
+	defer server.close()
+
+	// openssl req -newkey rsa:2048 -nodes -keyout online_key.pem -x509 -days 365 -out online_cert.pem -set_serial 1
+	onlineCert := `
+-----BEGIN CERTIFICATE-----
+MIIDTjCCAjagAwIBAgIBATANBgkqhkiG9w0BAQsFADBAMQswCQYDVQQGEwJVQTET
+MBEGA1UECAwKU29tZS1TdGF0ZTENMAsGA1UEBwwES3lpdjENMAsGA1UECgwERVBB
+TTAeFw0yMDA5MTAxNDE1MzNaFw0yMTA5MTAxNDE1MzNaMEAxCzAJBgNVBAYTAlVB
+MRMwEQYDVQQIDApTb21lLVN0YXRlMQ0wCwYDVQQHDARLeWl2MQ0wCwYDVQQKDARF
+UEFNMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvc1utrAEKlJ5rB9W
+envNiEjGDW36NKmEc69nITCmI7wedg00oRSIOZTfZVdKp/Nsh0uuFnOYKt9unXso
+fYGzCk4KxWL+t9HDsWMbpL7S/QNB1UF7P+rQFKp7gsj8wQmy+rpvRxwuTC7BfpJ9
+az9WoreF4W43m3zDF4lIetnFDilx1NBYBfvW7/KW3e/iJcjs8WPSQCDVU3rOhFRd
+8Y/qt4EOiJY5xTya0YFxgF37fKH/asy+ija54Wy7DhbLlkyE2JUqxp/SaomzUAQX
+uf/kWYPh/s7VewjfW9xouT9aDLZEsLNQMEk6HRPY9DW2pCdYFjE8qdVYAs/f0V5y
+QhPUaQIDAQABo1MwUTAdBgNVHQ4EFgQUCwq4ojzTld4lTka0POLVqgkyMJ4wHwYD
+VR0jBBgwFoAUCwq4ojzTld4lTka0POLVqgkyMJ4wDwYDVR0TAQH/BAUwAwEB/zAN
+BgkqhkiG9w0BAQsFAAOCAQEAIDdNqOeK4JyZtETgfwj5gqneZ9EH+euJ6HaOict8
+7PZzyEt2p0QHZOWBm0s07kd+7SiS+UCLIYjJPCMWDAEN7V6zZqqp9oQL5bR1hSSe
+7tDrOgVkEf8T1x4u4F2bpLJsz+GDX7t8H5lcvPGq0YkPDHluSASFJc//GN3TrSDV
+yGuI5R8+7XalVAaCteo/Y2zhERMDsVm0KTGeTP2sblaBVFux7GTcWA1DHqVDYFnw
+ExQScaNHy16y+a/nlDSpTraFIQdSG3twwlfyjR/Ua5SbkzzTEVG+Kll/3VvOagwV
+8xTUZedoJTC7G5C2DGs+syl/B8WVrn7QPK+VSVU2QEG50Q==
+-----END CERTIFICATE-----
+`
+
+	// openssl req -newkey rsa:2048 -nodes -keyout offline_key.pem -x509 -days 365 -out offline_cert.pem -set_serial 2
+	offlineCert := `
+-----BEGIN CERTIFICATE-----
+MIIDTjCCAjagAwIBAgIBAjANBgkqhkiG9w0BAQsFADBAMQswCQYDVQQGEwJVQTET
+MBEGA1UECAwKU29tZS1TdGF0ZTENMAsGA1UEBwwES3lpdjENMAsGA1UECgwERVBB
+TTAeFw0yMDA5MTAxNDE1NTdaFw0yMTA5MTAxNDE1NTdaMEAxCzAJBgNVBAYTAlVB
+MRMwEQYDVQQIDApTb21lLVN0YXRlMQ0wCwYDVQQHDARLeWl2MQ0wCwYDVQQKDARF
+UEFNMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoDW6XO7bHeTRnF10
+5FZ6wFKnyp0v6aACJxuWrLdOGlOZeaO/7q1I1qD5ogxV2Fh6IHm6+4e+wo4lLAUX
+F7Fi/ab/FZjHU6a2XNxNi6VnE7C7wp/UDt8/KfHV6ZzkyubHoraMl5Xi8g+3IN1N
+kTc90OCZPL6aZHpMXaP+5dOXad/Pv2/6xHKB/Qx+VIQlo1c7oGrYxFc29LsUtlrP
+C0tcDVAfMBBlRIhkw/dXoGcBSfqBIAnORYq/kGcbEewWDLdBiExr+TtJh837NgiE
+q1+NIvWF4AS3vL+tm/rvXfz71v3Gy4JqL/8Eqn489VE2Vw/XJfLjte8fPpKc5NVN
+Ah7bsQIDAQABo1MwUTAdBgNVHQ4EFgQUdjLMDiQH1r6a4ra3GX9hwdVzlWkwHwYD
+VR0jBBgwFoAUdjLMDiQH1r6a4ra3GX9hwdVzlWkwDwYDVR0TAQH/BAUwAwEB/zAN
+BgkqhkiG9w0BAQsFAAOCAQEAYCm82d7bMk3bROnNvIp/IcZdO6SItuhTy6iPQRd8
+ZqIIPsOG/8uMTKimCvJZIhsb+P9rRj3Ubb4EAAHpftZJx6Y1yrGlFUVYhsDNSsQR
+RqT4gg71B7R3Mgu0T9tV96nNa7P0w32wvNtc7B/it8DsFMz1wOcq/PPh3ufoR9Lm
+onsMCZ7ep/quYwXutmvMrE3SLGApTc7lnqPqBtVq4ju29VS6wDsvLgEyuZQO9HlJ
+KbjNq7kDX6ZbgJTVgVwEVY16464lTJ3j6/Osi3R3bUs5cg4onCFAS5KUTsfkbZ+G
+KzpDMr/kcScwzmmNcN8aLp31TSRVee64QrK7yF3YJxL+rA==
+-----END CERTIFICATE-----	
+`
+
+	if err = ioutil.WriteFile(path.Join(tmpDir, "online_cert.pem"), []byte(onlineCert), 0644); err != nil {
+		log.Fatalf("Error create online cert: %s", err)
+	}
+
+	if err = ioutil.WriteFile(path.Join(tmpDir, "offline_cert.pem"), []byte(offlineCert), 0644); err != nil {
+		log.Fatalf("Error create offline cert: %s", err)
+	}
+
+	onlineURL := url.URL{Scheme: "file", Path: path.Join(tmpDir, "online_cert.pem")}
+	offlineURL := url.URL{Scheme: "file", Path: path.Join(tmpDir, "offline_cert.pem")}
+
+	server.certURL = map[string]string{
+		"online":  onlineURL.String(),
+		"offline": offlineURL.String()}
+
+	client, err := cmclient.New(&config.Config{CMServerURL: serverURL}, sender, true)
+	if err != nil {
+		t.Fatalf("Can't create CM client: %s", err)
+	}
+	defer client.Close()
+
+	certInfo := []amqp.IssuedUnitCertificatesInfo{
+		{Type: "online", CertificateChain: "onlineCert"},
+		{Type: "offline", CertificateChain: "offlineCert"},
+	}
+
+	if err = client.InstallCertificates(certInfo); err != nil {
+		t.Fatalf("Can't process install certificates request: %s", err)
+	}
+
+	serial := map[string]string{"online": "1", "offline": "2"}
+
+	if !reflect.DeepEqual(serial, sender.serial) {
+		t.Errorf("Wrong sender CSR: %v", sender.serial)
+	}
+}
+
+func TestGetCertificates(t *testing.T) {
+	sender := &testSender{}
+
+	server, err := newTestServer(serverURL)
+	if err != nil {
+		t.Fatalf("Can't create test server: %s", err)
+	}
+	defer server.close()
+
+	server.certURL = map[string]string{"online": "onlineCertURL", "offline": "offlineCertURL"}
+	server.keyURL = map[string]string{"online": "onlineKeyURL", "offline": "offlineKeyURL"}
+
+	client, err := cmclient.New(&config.Config{CMServerURL: serverURL}, sender, true)
+	if err != nil {
+		t.Fatalf("Can't create CM client: %s", err)
+	}
+	defer client.Close()
+
+	for serial, certType := range []string{"online", "offline"} {
+		certURL, keyURL, err := client.GetCertificate(certType, nil, strconv.Itoa(serial))
+		if err != nil {
+			t.Errorf("Can't get %s certificate: %s", certType, err)
+			continue
+		}
+
+		if certURL != server.certURL[certType] {
+			t.Errorf("Wrong %s cert URL: %s", certType, certURL)
+		}
+
+		if keyURL != server.keyURL[certType] {
+			t.Errorf("Wrong %s key URL: %s", certType, keyURL)
+		}
+	}
+}
+
+/*******************************************************************************
+ * Private
+ ******************************************************************************/
+
+func newTestServer(url string) (server *testServer, err error) {
+	server = &testServer{}
+
+	listener, err := net.Listen("tcp", url)
+	if err != nil {
+		return nil, err
+	}
+	server.grpcServer = grpc.NewServer()
+
+	pb.RegisterCertificateManagerServer(server.grpcServer, server)
+
+	go server.grpcServer.Serve(listener)
+
+	return server, nil
+}
+
+func (server *testServer) close() (err error) {
+	if server.grpcServer != nil {
+		server.grpcServer.Stop()
+	}
+
+	return nil
+}
+
+func (server *testServer) CreateKeys(context context.Context, req *pb.CreateKeysReq) (rsp *pb.CreateKeysRsp, err error) {
+	rsp = &pb.CreateKeysRsp{Type: req.Type}
+
+	csr, ok := server.csr[req.Type]
+	if !ok {
+		rsp.Error = "not found"
+	}
+
+	rsp.Csr = csr
+
+	return rsp, nil
+}
+
+func (server *testServer) ApplyCert(context context.Context, req *pb.ApplyCertReq) (rsp *pb.ApplyCertRsp, err error) {
+	rsp = &pb.ApplyCertRsp{Type: req.Type}
+
+	certURL, ok := server.certURL[req.Type]
+	if !ok {
+		rsp.Error = "not found"
+	}
+
+	rsp.CertUrl = certURL
+
+	return rsp, nil
+}
+
+func (server *testServer) GetCert(context context.Context, req *pb.GetCertReq) (rsp *pb.GetCertRsp, err error) {
+	rsp = &pb.GetCertRsp{Type: req.Type}
+
+	certURL, ok := server.certURL[req.Type]
+	if !ok {
+		rsp.Error = "not found"
+	}
+
+	keyURL, ok := server.keyURL[req.Type]
+	if !ok {
+		rsp.Error = "not found"
+	}
+
+	rsp.CertUrl = certURL
+	rsp.KeyUrl = keyURL
+
+	return rsp, nil
+}
+
+func (sender *testSender) SendIssueUnitCertificatesRequest(requests []amqp.CertificateRequest) (err error) {
+	sender.csr = make(map[string]string)
+
+	for _, request := range requests {
+		sender.csr[request.Type] = request.Csr
+	}
+
+	return nil
+}
+
+func (sender *testSender) SendInstallCertificatesConfirmation(confirmations []amqp.CertificateConfirmation) (err error) {
+	sender.serial = make(map[string]string)
+
+	for _, confirmation := range confirmations {
+		sender.serial[confirmation.Type] = confirmation.Serial
+	}
+
+	return nil
+}
