3ed813a29d87e90a27d2070f5276ab2025c35b7c
Yevgen Abramov <Yevgen_Abramov@epam.com>
2021-02-01 09:59:56 +0200

    [ci] Update script to prepare ci server
    Signed-off-by: Yevgen Abramov <Yevgen_Abramov@epam.com>

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,32 +1,51 @@
 #!/bin/sh
 
-echo "Install  Golang v 1.14"
+echo "Install  gitlab runner"
+curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+sudo dpkg -i gitlab-runner_amd64.deb
+sudo usermod -a -G sudo gitlab-runner
+#sudo visudo
+#gitlab-runner ALL=(ALL) NOPASSWD: ALL
 
-curl https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz --output /tmp/go.tar.gz
+echo "Install Docker"
+sudo apt install apt-transport-https  ca-certificates curl  gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+#privileged = true
+#volumes = ["/cache", "/dev:/dev"] - for docker ruuners in /etc/gitlab-runner/config.toml
+
+
+echo "Install  Golang v 1.14"
+curl -L https://golang.org/dl/go1.14.14.linux-amd64.tar.gz --output /tmp/go.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go.tar.gz
 echo  PATH=$PATH:/usr/local/go/bin >> $HOME/.profile
-
 mkdir -p $HOME/go/src
 
-echo "Install SM dependencies"
-sudo apt update -y
-sudo apt install libsystemd-dev dbus-x11 make iperf quota runc -y
+echo "Install  CNI"
+git clone https://github.com/containernetworking/plugins.git
+cd  plugins
+./build_linux.sh
+sudo mkdir /opt/cni
+sudo mv ./bin/ /opt/cni/
 
-echo "Install wondershaper"
-cd /tmp
-git clone  https://github.com/magnific0/wondershaper.git
-cd wondershaper
-sudo make install
+echo "Install SM dependencies"
+sudo apt install quota dnsmasq runc -y
 
 echo "Install tests dependencies"
-sudo apt install libssl-dev -y
-sudo apt install rabbitmq-server -y
+sudo apt install libsystemd-dev dbus-x11 make iperf libssl-dev rabbitmq-server -y
+sudo systemctl stop rabbitmq-server
 sudo systemctl disable rabbitmq-server
 
-sudo apt install python3-pip -y
-sudo -H pip3 install pyftpdlib
+-echo "Install wondershaper"
+-cd /tmp
+-git clone  https://github.com/magnific0/wondershaper.git
+-cd wondershaper
+-sudo make install
 
-echo "Install gitlab runner dependencies"
-curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
-sudo apt-get install gitlab-runner
-sudo usermod -a -G sudo gitlab-runner
+echo "Install python3.7 and libs"
+sudo apt install python3-pip  -y
+sudo -H pip3 install pyftpdlib
+sudo apt install python3.7
