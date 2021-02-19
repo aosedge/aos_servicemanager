10e7fcff63f3dd0f2fdaf2dc2683f4fb230c6a1b
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2021-02-19 13:56:46 +0200

    [vendor] Update vendor to use aoc_common cryptutils and tpmkey packages
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	github.com/godbus/dbus v4.1.0+incompatible
 	github.com/golang/protobuf v1.4.3
 	github.com/google/go-tpm v0.3.2
-	github.com/google/go-tpm-tools v0.2.1
+	github.com/google/go-tpm-tools v0.2.1 // indirect
 	github.com/google/uuid v1.1.5
 	github.com/hashicorp/go-version v1.2.1 // indirect
 	github.com/jinzhu/copier v0.2.3
@@ -30,13 +30,14 @@ require (
 	github.com/opencontainers/image-spec v1.0.1
 	github.com/opencontainers/runc v1.0.0-rc92.0.20210111190311-9712205ecc3a
 	github.com/opencontainers/runtime-spec v1.0.3-0.20200929063507-e6143ca7d51d
+	github.com/pkg/errors v0.9.1
 	github.com/shirou/gopsutil v3.20.12+incompatible
 	github.com/sirupsen/logrus v1.7.0
 	github.com/speijnik/go-errortree v1.0.1 // indirect
 	github.com/streadway/amqp v1.0.0
 	github.com/vishvananda/netlink v1.1.1-0.20201029203352-d40f9887b852
 	github.com/vishvananda/netns v0.0.0-20200728191858-db3c7e526aae
-	gitpct.epam.com/epmd-aepr/aos_common v0.0.0-20210113091140-a6a0b6488575
+	gitpct.epam.com/epmd-aepr/aos_common v0.0.0-20210219143607-3d89a40326f0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d
 	google.golang.org/grpc v1.35.0
---
+++ vendor/gitpct.epam.com/epmd-aepr/aos_common/utils/tpmkey/tpmkey.go
@@ -0,0 +1,211 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2021 Renesas Inc.
+// Copyright 2021 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tpmkey
+
+import (
+	"crypto"
+	"encoding/asn1"
+	"errors"
+	"io"
+	"math/big"
+
+	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpmutil"
+)
+
+/*******************************************************************************
+ * Consts
+ ******************************************************************************/
+
+const tpmOpenRetry = 3
+
+/*******************************************************************************
+ * Types
+ ******************************************************************************/
+
+// TPMKey TPM key instance
+type TPMKey interface {
+	Password() (password string)
+	MakePersistent(persistentHandle tpmutil.Handle) (err error)
+}
+
+type tpmKey struct {
+	device           io.ReadWriter
+	primaryHandle    tpmutil.Handle
+	persistentHandle tpmutil.Handle
+	publicKey        crypto.PublicKey
+	privateBlob      []byte
+	publicBlob       []byte
+	password         string
+}
+
+type rsaKey struct {
+	tpmKey
+}
+
+type eccKey struct {
+	tpmKey
+}
+
+/*******************************************************************************
+ * Vars
+ ******************************************************************************/
+
+var supportedHash = map[crypto.Hash]tpm2.Algorithm{
+	crypto.SHA1:   tpm2.AlgSHA1,
+	crypto.SHA256: tpm2.AlgSHA256,
+	crypto.SHA384: tpm2.AlgSHA384,
+	crypto.SHA512: tpm2.AlgSHA512,
+}
+
+/*******************************************************************************
+ * Public
+ ******************************************************************************/
+
+// CreateFromPersistent creates key from persistent storage
+func CreateFromPersistent(device io.ReadWriter, persistentHandle tpmutil.Handle) (key TPMKey, err error) {
+	tpmPublic, _, _, err := tpm2.ReadPublic(device, persistentHandle)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKey, err := tpmPublic.Key()
+	if err != nil {
+		return nil, err
+	}
+
+	return createNewKey(tpmPublic.Type, tpmKey{
+		device:           device,
+		persistentHandle: persistentHandle,
+		publicKey:        publicKey,
+	})
+}
+
+// CreateFromBlobs creates key from blobs
+func CreateFromBlobs(device io.ReadWriter, primaryHandle tpmutil.Handle,
+	password string, privateBlob, publicBlob []byte) (key TPMKey, err error) {
+	tpmPublic, err := tpm2.DecodePublic(publicBlob)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKey, err := tpmPublic.Key()
+	if err != nil {
+		return nil, err
+	}
+
+	return createNewKey(tpmPublic.Type, tpmKey{
+		device:        device,
+		primaryHandle: primaryHandle,
+		password:      password,
+		privateBlob:   privateBlob,
+		publicBlob:    publicBlob,
+		publicKey:     publicKey,
+	})
+}
+
+/*******************************************************************************
+ * Private
+ ******************************************************************************/
+
+func createNewKey(algorithm tpm2.Algorithm, tpmKey tpmKey) (key TPMKey, err error) {
+	switch algorithm {
+	case tpm2.AlgRSA:
+		return &rsaKey{tpmKey: tpmKey}, nil
+
+	case tpm2.AlgECC:
+		return &eccKey{tpmKey: tpmKey}, nil
+
+	default:
+		return nil, errors.New("unsupported key type")
+	}
+}
+
+func makePersistent(key *tpmKey, persistentHandle tpmutil.Handle) (err error) {
+	if key.persistentHandle != 0 {
+		return errors.New("key already in persistent storage")
+	}
+
+	keyHandle, _, err := tpm2.Load(key.device, key.primaryHandle, key.password, key.publicBlob, key.privateBlob)
+	if err != nil {
+		return err
+	}
+	defer tpm2.FlushContext(key.device, keyHandle)
+
+	// Clear slot
+	tpm2.EvictControl(key.device, key.password, tpm2.HandleOwner, persistentHandle, persistentHandle)
+
+	if err = tpm2.EvictControl(key.device, key.password, tpm2.HandleOwner, keyHandle, persistentHandle); err != nil {
+		return err
+	}
+
+	key.persistentHandle = persistentHandle
+
+	return nil
+}
+
+func sign(key tpmKey, digest []byte, scheme tpm2.SigScheme) (signature []byte, err error) {
+	var keyHandle tpmutil.Handle
+
+	if key.persistentHandle == 0 {
+		if keyHandle, _, err = tpm2.Load(key.device, key.primaryHandle, key.password,
+			key.publicBlob, key.privateBlob); err != nil {
+			return nil, err
+		}
+		defer tpm2.FlushContext(key.device, keyHandle)
+	} else {
+		keyHandle = key.persistentHandle
+	}
+
+	sig, err := tpm2.Sign(key.device, keyHandle, "", digest, nil, &scheme)
+	if err != nil {
+		return nil, err
+	}
+
+	switch sig.Alg {
+	case tpm2.AlgRSASSA:
+		return sig.RSA.Signature, nil
+
+	case tpm2.AlgRSAPSS:
+		return sig.RSA.Signature, nil
+
+	case tpm2.AlgECDSA:
+		sigStruct := struct{ R, S *big.Int }{sig.ECC.R, sig.ECC.S}
+
+		return asn1.Marshal(sigStruct)
+
+	default:
+		return nil, errors.New("unsupported signing algorithm")
+	}
+}
+
+func decryptRSA(key tpmKey, msg []byte, scheme tpm2.AsymScheme, label string) (plaintext []byte, err error) {
+	var keyHandle tpmutil.Handle
+
+	if key.persistentHandle == 0 {
+		if keyHandle, _, err = tpm2.Load(key.device, key.primaryHandle, key.password,
+			key.publicBlob, key.privateBlob); err != nil {
+			return nil, err
+		}
+		defer tpm2.FlushContext(key.device, keyHandle)
+	} else {
+		keyHandle = key.persistentHandle
+	}
+
+	return tpm2.RSADecrypt(key.device, keyHandle, "", msg, &scheme, label)
+}
