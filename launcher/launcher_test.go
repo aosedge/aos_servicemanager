0969c036f5ba2238b2b7f02508249f9f253c0d35
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2022-01-10 14:21:28 +0200

    [launcher] Add new interface for subjects functionality
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ launcher/launcher_test.go
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright (C) 2022 Renesas Electronics Corporation.
+// Copyright (C) 2022 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package launcher_test
+
+import (
+	"os"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/aoscloud/aos_servicemanager/config"
+	"github.com/aoscloud/aos_servicemanager/launcher"
+)
+
+/***********************************************************************************************************************
+ * Init
+ **********************************************************************************************************************/
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: false,
+		TimestampFormat:  "2006-01-02 15:04:05.000",
+		FullTimestamp:    true,
+	})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+}
+
+/***********************************************************************************************************************
+ * Tests
+ **********************************************************************************************************************/
+
+func TestLauncher(t *testing.T) {
+	instance, err := launcher.New(&config.Config{}, nil, nil, nil)
+	if err != nil {
+		t.Fatalf("Can't create launcher: %s", err)
+	}
+	defer instance.Close()
+}
