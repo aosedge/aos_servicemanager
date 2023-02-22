9ce3dc9f0d475a71df3e55f37cc0149dac4179c3
Mykola Solianko <mykola_solianko@epam.com>
2023-02-22 10:00:05 +0200

    [networkmanager] Use predefined IP and subnet
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- networkmanager/netutils.go
+++ networkmanager/netutils.go
@@ -19,44 +19,14 @@
 package networkmanager
 
 import (
-	"net"
 	"os"
 	"path"
-	"reflect"
 	"runtime"
-	"syscall"
 
 	"github.com/aoscloud/aos_common/aoserrors"
-	"github.com/apparentlymart/go-cidr/cidr"
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
 )
-
-func getNetworkRoutes() (routeIPList []netlink.Route, err error) {
-	initNl, err := netlink.NewHandle(syscall.NETLINK_ROUTE, syscall.NETLINK_NETFILTER)
-	if err != nil {
-		return nil, aoserrors.Errorf("could not create netlink handle on initial namespace: %v", err)
-	}
-
-	defer initNl.Close()
-
-	routeIPList, err = initNl.RouteList(nil, netlink.FAMILY_V4)
-	if err != nil {
-		return nil, aoserrors.Wrap(err)
-	}
-
-	return routeIPList, nil
-}
-
-func checkRouteOverlaps(toCheck *net.IPNet, networks []netlink.Route) (overlapsIPs bool) {
-	for _, network := range networks {
-		if network.Dst != nil && (toCheck.Contains(network.Dst.IP) || network.Dst.Contains(toCheck.IP)) {
-			return true
-		}
-	}
-
-	return false
-}
 
 func removeBridgeInterface(spID string) (err error) {
 	br, err := netlink.LinkByName(bridgePrefix + spID)
@@ -110,37 +80,4 @@ func createNetNS(name string) (err error) {
 	}
 
 	return nil
-}
-
-func getIPAddressRange(subnetwork *net.IPNet) (ipLowNetRange net.IP, ipHighNetRange net.IP) {
-	minIPRange, maxIPRange := cidr.AddressRange(subnetwork)
-
-	return cidr.Inc(minIPRange), cidr.Dec(maxIPRange)
-}
-
-func checkExistNetInterface(name string) (ipNet *net.IPNet, err error) {
-	netInterface, err := net.InterfaceByName(name)
-	if err != nil {
-		return nil, aoserrors.Errorf("unable to find interface %s", err)
-	}
-
-	addrs, err := netInterface.Addrs()
-	if err != nil {
-		return nil, aoserrors.Errorf("interface has no address %s", err)
-	}
-
-	for _, addr := range addrs {
-		switch v := addr.(type) {
-		case *net.IPNet:
-			if ipv4 := v.IP.To4(); ipv4 != nil {
-				_, ipSubnet, _ := net.ParseCIDR(v.String())
-				return ipSubnet, nil
-			}
-
-		default:
-			return nil, aoserrors.Errorf("unsupported key type: %v", reflect.TypeOf(v))
-		}
-	}
-
-	return nil, aoserrors.Errorf("interface has not IPv4 address")
 }
